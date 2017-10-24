e802f8cea34aa2716442b1f898471ede80b481a8
Lucas Luchiari <lluchiari@localhost.localdomain>
2017-10-24 16:40:43 -0200

    Finish section 7

---
+++ src/containers/AlbumDetail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Card from './Card'
+import CardSection from './CardSection';
+
+
+const AlbumDetail = (props) => {
+  return (
+    <Card>
+      <CardSection>
+        <Text> {props.data.title} </Text>
+      </CardSection>
+
+    </Card>
+  );
+
+};
+
+export default AlbumDetail;
